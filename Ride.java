f19b9edad6f391de0eb4b06fc7b308cae3853e5a
nayan-bansal <nayan.7991@gmail.com>
2020-10-24 11:36:25 +0530

    Added: File for Multiple Rides

---
+++ Ride.java
@@ -0,0 +1,14 @@
+package Cab.Cab_Invoice_System;
+
+public class Ride {
+
+	public double distance;
+	public int time;
+
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+	
+	
+}
