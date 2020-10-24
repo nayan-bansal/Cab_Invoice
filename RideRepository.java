3af45125e58cb8c8c455d2883e566d2e9d88bb9e
nayan-bansal <nayan.7991@gmail.com>
2020-10-24 13:11:55 +0530

    Added: Ride Repository

---
+++ RideRepository.java
@@ -0,0 +1,18 @@
+package Cab.Cab_Invoice_System;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RideRepository {
+	
+	private Map<Integer, Ride[]> USER_RIDES = new HashMap<Integer, Ride[]>();
+
+	public RideRepository(int userId, Ride[] rides) {
+		USER_RIDES.put(userId, rides);
+	}
+
+	public Ride[] getRides(int userId) {
+		return USER_RIDES.get(userId);
+	}
+
+}
