586ccb201b0fe69cd381d273b8ec2df841af7746
nayan-bansal <nayan.7991@gmail.com>
2020-10-24 12:34:28 +0530

    Added: Junit File for Cab Invoice Program

--- Invoice_MainTest.java
+++ Invoice_MainTest.java
@@ -8,18 +8,18 @@ import junit.framework.Assert;
 public class Invoice_Main_Test {
 
 	
-	Invoice_Main invoice = null;
+	Invoice_Main invoiceGenerator = null;
 
 	@Before
 	public void setUp() throws Exception {
-		Invoice_Main Invoice_Main = new Invoice_Main();
+		invoiceGenerator = new Invoice_Main();
 	}
 	
 	@Test
 	public void givenDistanceAndTimeShouldReturnTotalFare() {
 		double distance = 2.5;
 		int time = 10;
-		double fare = Invoice_Main.calculateFare(distance, time);
+		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(35, fare, 0.0);
 	}
 
@@ -27,15 +27,16 @@ public class Invoice_Main_Test {
 	public void givenLessDistanceAndTimeShouldReturnMinimumFare() {
 		double distance = 0.1;
 		int time = 1;
-		double fare = Invoice_Main.calculateFare(distance, time);
+		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(5, fare, 0.0);
 	}
 	
 	@Test
-	public void givenMultipleRidesShouldReturnAggregateTotalForAll() {
+	public void givenMultipleRidesShouldReturnInvoiceSummary() {
 		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
-		double totalFare = Invoice_Main.calculateFare(rides);
-		Assert.assertEquals(30, totalFare, 0.0);
+		InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rides);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+		Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
 	}
 	
 }
