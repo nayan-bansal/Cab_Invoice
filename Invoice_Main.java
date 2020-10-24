2ba0720444cd8cfa2c739db5fc4c14f21494ee51
nayan-bansal <nayan.7991@gmail.com>
2020-10-24 13:11:12 +0530

    Added: Main File  for Invocie Service

--- Invoice_Main.java
+++ Invoice_Main.java
@@ -7,6 +7,15 @@ public class Invoice_Main {
 	private static final double MINIMUM_COST_PER_KM = 10.0;
 	private static final int COST_PER_TIME = 1;
 	private static final double MINIMUM_FARE = 5.0;
+	private final RideRepository USER_RIDES;
+	
+	public Invoice_Main() {
+		this.USER_RIDES = null;
+	}
+
+	public Invoice_Main(int userId, Ride[] rides) {
+		USER_RIDES = new RideRepository(userId, rides);
+	}
 	
 	public static void main(String[] args) {
 		Logger log = Logger.getLogger(Invoice_Main.class.getName());
@@ -17,8 +26,9 @@ public class Invoice_Main {
 		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
 		return Math.max(totalFare, MINIMUM_FARE);
 	}
-	
-	public InvoiceSummary calculateFare(Ride[] rides) {
+
+	public InvoiceSummary calculateFare(int userId) {
+		Ride[] rides = USER_RIDES.getRides(userId);
 		double totalFare = 0.0;
 		for (Ride ride : rides) {
 			totalFare += this.calculateFare(ride.distance, ride.time);
