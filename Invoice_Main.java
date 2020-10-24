32190619fbdfab78e1b5095c2ba4e459343753bf
nayan-bansal <nayan.7991@gmail.com>
2020-10-24 11:38:35 +0530

    Added: Main file for Multiple Rides

--- Invoice_Main.java
+++ Invoice_Main.java
@@ -12,12 +12,20 @@ public class Invoice_Main {
 	
 	public static void main (String[] args) {
 		Logger Log = Logger.getLogger(Invoice_Main.class.getName());
-		Log.info("Welcome to Cab Invoice Generation System");	
+		Log.info("Welcome to Can Invoice Generation System");	
 	}
 	
 	public static double calculateFare(double distance, int time) {
 		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
 		return Math.max(totalFare, MINIMUM_FARE);
+	}
+	
+	public static double calculateFare(Ride[] rides) {
+		double totalFare = 0.0;
+		for (Ride ride : rides) {
+			totalFare += calculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
 	}
 	
 }
